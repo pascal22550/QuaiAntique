bd3dad93d2f349d5cc8667b5580179cedec93b5b
Pascal <madinfopc@gmail.com>
2025-08-28 20:25:14 +0200

    Mise en place de la page connexion/inscription

--- QuaiAntique/Router/allRoutes.js
+++ QuaiAntique/Router/allRoutes.js
@@ -3,7 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
+
