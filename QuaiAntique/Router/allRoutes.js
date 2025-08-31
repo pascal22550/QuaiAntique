1eae6ce22101123fb989b9ff6508116494d068d0
Pascal <madinfopc@gmail.com>
2025-08-31 18:24:03 +0200

    AffichageReservation

--- QuaiAntique/Router/allRoutes.js
+++ QuaiAntique/Router/allRoutes.js
@@ -7,7 +7,8 @@ export const allRoutes = [
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/editPassord", "Changement du mot de passe", "/pages/auth/editPassword.html")
+    new Route("/editPassord", "Changement du mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html")
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
